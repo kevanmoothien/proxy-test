1f03978888f6b1962c03094768922f5c87d3dad2
Kevan <kevan.moothien@gmail.com>
2025-03-05 20:04:29 +0400

    replace index js

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-const TARGET_SERVER = 'https://kevan.in.ngrok.io'; // Replace with your target server
+const TARGET_SERVER = 'https://annuaire-uat.petitsfreresdespauvres.fr'; // Replace with your target server
 
-// Proxy middleware to forward requests
+// https://annuaire-uat.petitsfreresdespauvres.fr/services/api/authenticate
+
+// Load SSL certificate, private key, and CA certificate
+const httpsAgent = new https.Agent({
+    cert: fs.readFileSync('cert.pem'),
+    key: fs.readFileSync('key.pem'),
+    ca: fs.readFileSync('root.pem') // Optional, if CA is required
+});
+
+// Proxy middleware with HTTPS certificate
 app.use(
-    '/', // Forward all requests
+    '/',
     createProxyMiddleware({
         target: TARGET_SERVER,
         changeOrigin: true,
+        agent: httpsAgent, // Use the custom agent with the certificate
         onProxyReq: (proxyReq, req, res) => {
             console.log(`Proxying request: ${req.method} ${req.url}`);
         },
